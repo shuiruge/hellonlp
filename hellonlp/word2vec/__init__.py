8c521622e8108a402e749c4ffca195c0edd6ca02
pen. ciun. jian <shuiruge@whu.edu.cn>
2019-02-17 14:09:50 +0800

    Update __init__.py

--- hellonlp/word2vec/__init__.py
+++ hellonlp/word2vec/__init__.py
@@ -1,5 +1,10 @@
-r"""Implements the word2vec model by Python with the aid of numpy.
+r"""
+Description
+-----------
+Implements the word2vec model by Python with the aid of numpy.
 
-The original C languange implementation with comments locates at
-[here](https://github.com/chrisjmccormick/word2vec_commented/blob/master/word2vec.c).
+References
+----------
+1. The paper (arXiv: 1310.4546) is [herein](https://arxiv.org/pdf/1310.4546.pdf).
+1. The original C languange implementation with comments locates at [here](https://github.com/chrisjmccormick/word2vec_commented/blob/master/word2vec.c).
 """
